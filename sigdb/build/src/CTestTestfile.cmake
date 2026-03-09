5c619a4df8d2afbbe3322b02b492903065977888
rtgthedestroyer <arielweiser21@gmail.com>
2026-03-09 21:13:54 +0200

    file skeleton

---
+++ sigdb/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joe/repos/sigdb/sigdb/src
+# Build directory: /home/joe/repos/sigdb/sigdb/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ sigdb/build/tools/CMakeFiles/sdb.dir/sdb.cpp.o.d
@@ -0,0 +1,3 @@
+tools/CMakeFiles/sdb.dir/sdb.cpp.o: \
+ /home/joe/repos/sigdb/sigdb/tools/sdb.cpp /usr/include/stdc-predef.h \
+ /home/joe/repos/sigdb/sigdb/include/libsdb/libsdb.hpp
