373a0e29dac44125f273ff792c4df06b16032f90
rtgthedestroyer <arielweiser21@gmail.com>
2026-03-12 16:33:44 +0200

    added a protoype of attaching to a process

--- sigdb/tools/sdb.cpp
+++ sigdb/tools/sdb.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <string_view>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 namespace{
     // attach to a process 
@@ -24,17 +26,44 @@ namespace{
         }
         
         else{
-
+            const char* program_path = argv[1];
+            if((pid=fork())<0){
+                std::perror("fork failed");
+                return -1;
+            }
+            //checks if we are the child process
+            if(pid==0){
+                //allow to send more ptrace requests in the future
+                if(ptrace(PTRACE_TRACEME,0,nullptr,nullptr)<0){
+                    std::perror("tracing failed");
+                    return -1;
+            }
+            if(execlp(program_path, program_path, nullptr)<0){
+                std::perror("exec failed");
+                return -1;
+            }
         }
 
         return pid;
     }
 }
 int main(int argc, const char** argv){
+    //no args
     if(argc==1){
         std::cerr << "not args given\n";
         return -1;
     }
 
+
     pid_t pid = attach(argc, argv);
+
+    int wait_status;
+    int options = 0;
+
+    //wait for the process to stop after attaching to it
+    if(waitpid(pid,&wait_status,options)<0){
+        std:perror("waitpid failed");
+    }
+
+    
 }
